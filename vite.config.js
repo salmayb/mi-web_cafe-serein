21ae4c37381f942769b38a3a7527b89d3ec7c6e9
Salma <salmayb28@gmail.com>
2026-06-17 19:46:39 +0200

    configuracion github pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite' 
-import react from '@vitejs/plugin-react' 
-import tailwindcss from '@tailwindcss/vite' 
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: "/cafe-serein/",
+plugins: [react(), tailwindcss()],
+base: "/mi-web_cafe-serein/",
 })
